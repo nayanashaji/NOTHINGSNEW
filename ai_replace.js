b5f51dc6e10d89fc5f9f64a5fa507bf021b36d78
nayanashaji <nayanashaji17102005@gmail.com>
2025-03-08 03:21:51 +0530

    timer

--- ai_replace.js
+++ ai_replace.js
@@ -4,9 +4,10 @@ async function fetch_synonym(word)
     {
         let response=await fetch(`https://api.datamuse.com/words?ml=${word}&max=5`);
         let data = await response.json();
-        if(data.length>0)
-        {
-            return data[Math.floor(Math.random()*data.length)].word;  
+        let singleWordSynonyms = data.filter(entry => !entry.word.includes(" ") && !entry.word.includes("-"));
+
+        if (singleWordSynonyms.length > 0) {
+            return singleWordSynonyms[Math.floor(Math.random() * singleWordSynonyms.length)].word;
         }
     }
     catch (error)
@@ -21,7 +22,7 @@ async function fetch_synonym(word)
     {
         if(index<words.length-1)
             {
-                if(Math.random()<0.4)
+                if(Math.random()<0.45)
                 {
                     let new_word=await fetch_synonym(words[index+1]);
                     words[index+1]=new_word;
